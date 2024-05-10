a39e81d16a7f254370d792e9d5b9e44b4a67da0b
programmingKyle <155344874+programmingKyle@users.noreply.github.com>
2024-05-10 15:12:38 -0400

    Frame Maximize
    - Returned the maximize button!

--- src/frame.js
+++ src/frame.js
@@ -1,15 +1,15 @@
 const minimizeButton_el = document.getElementById('minimizeButton');
-//const maximizeButton_el = document.getElementById('maximizeButton');
+const maximizeButton_el = document.getElementById('maximizeButton');
 const exitButton_el = document.getElementById('exitButton');
 
 minimizeButton_el.addEventListener('click', () => {
     api.frameHandler({request: 'Minimize'});
 });
 
-/*maximizeButton_el.addEventListener('click', () => {
+maximizeButton_el.addEventListener('click', () => {
     api.frameHandler({request: 'Maximize'});
 });
-*/
+
 
 exitButton_el.addEventListener('click', () => {
     api.frameHandler({request: 'Exit'});
